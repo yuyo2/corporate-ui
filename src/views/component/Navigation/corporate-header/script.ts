f9201742248f864bfc5f7374d43c91fdc5f0c5b7
mohammed-softordi <mohammed.ayowa@softordi.com>
2018-04-25 10:37:35 +0200

    Bugfix

--- src/views/component/Navigation/corporate-header/script.ts
+++ src/views/component/Navigation/corporate-header/script.ts
@@ -14,8 +14,10 @@ Polymer({
   },
   created: function() {
     /* What this does is to visualize whatever nav-item will do if it was not yet loaded.
-      We might wanna add same thing to corporate-footer later if we wanna keep using Polymer... */
-    if (window['params'].preload === 'false') {
+      We might wanna add same thing to corporate-footer later if we wanna keep using Polymer...
+      TODO: mohammed - Not necessary any more as we will load components when and where needed if we are using the npm-package. For the CDN all components will be concatenated into a full version av CorporateUI
+      */
+    if (window['params'] && window['params'].preload === 'false') {
       var items = this.querySelectorAll('nav-item');
       for(var i=0; i<items.length; i++) {
         var item = items[i];
@@ -26,7 +28,7 @@ Polymer({
     }
   },
   ready: function() {
-    if (window['params'].preload !== 'false') {
+    if (window['params'] && window['params'].preload !== 'false') {
       var url = this.resolveUrl('../../Bootstrap/navbar/nav-item/nav-item.html');
       this.importHref(url);
     }
